61b22e81e0976472792371b145f24295ca1a1cbc
siegelverse <msiegel37@gmail.com>
2020-09-25 12:00:31 -0500

    submission for deadline

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,19 @@
 class HeroinesController < ApplicationController
+  before_action :current_heroine, only: [:show]
   def index
     @heroines = Heroine.all
   end
+
+  def show 
+  end 
+
+  def current_heroine 
+    @heroine = Heroine.find(params[:id])
+  end 
+
+  private 
+
+  def heroine_params 
+    params.require(:heroine).permit(:name, :super_name)
+  end 
 end
